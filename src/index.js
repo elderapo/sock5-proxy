d2f12158e1c7c15a8059057fe26528cff581c391
Tomasz Martyński <tomek@martynski.pl>
2020-01-22 21:56:07 +0100

    add multi host ip support

--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
 const socks = require("socksv5");
+const os = require("os");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 1080;
 const LOGIN = process.env.LOGIN;
 const PASSWORD = process.env.PASSWORD;
 
-let isAuthUsed = false;
-const server = socks.createServer((info, accept, deny) => {
-  console.log("New connection:", info);
-  accept();
-});
+console.log(`Credintials: ${LOGIN}:${PASSWORD}`);
 
-server.listen(PORT, err => {
-  if (err) {
-    return console.error(err);
+const createServer = ip => {
+  const server = socks.createServer((info, accept, deny) => {
+    if (ip) {
+      info.localAddress = ip;
+    }
+    console.log("New connection:", info);
+    accept();
+  });
+
+  if (LOGIN && PASSWORD) {
+    isAuthUsed = true;
+    server.useAuth(
+      socks.auth.UserPassword((user, password, cb) => {
+        cb(user === LOGIN && password === PASSWORD);
+      })
+    );
+  } else {
+    console.error("Set LOGIN & PASSWORD env variables!");
+    process.exit(1);
   }
-  console.log(`SOCKS server is listening on port: ${PORT}!`);
-  console.log(`Credintials: ${LOGIN}:${PASSWORD}`);
-});
 
-if (LOGIN && PASSWORD) {
-  isAuthUsed = true;
-  server.useAuth(
-    socks.auth.UserPassword((user, password, cb) => {
-      cb(user === LOGIN && password === PASSWORD);
-    })
-  );
-} else {
-  console.error("Set LOGIN & PASSWORD env variables!");
-  process.exit(1);
+  server.listen(PORT, ip, err => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(`SOCKS server is listening on port: ${ip}:${PORT}!`);
+  });
+};
+
+const interfaces = os.networkInterfaces();
+
+for (const interfaceName of Object.keys(interfaces)) {
+  const interface = interfaces[interfaceName];
+
+  for (const info of interface) {
+    if (info.family !== "IPv4") {
+      continue;
+    }
+
+    createServer(info.address);
+  }
 }
